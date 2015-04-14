ed6b7adbcb9932dd651158a4a5d328c646599448
yizuishi <yizuishi@iti.co.jp>
2015-04-14 09:50:37 +0900

    fitst commit

---
+++ test/sample/jenkins/MainTest.java
@@ -0,0 +1,27 @@
+package sample.jenkins;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+public class MainTest {
+
+    @Test
+    public void repeatƒƒ\ƒbƒh‚Éhoge‚Æ5‚ğ“n‚µ‚½‚ç_hogehogehogehogehoge_‚ª•Ô‚Á‚Ä‚­‚é‚±‚Æ() {
+        // exercise
+        String actual = Main.repeat("hoge", 5);
+
+        // verify
+        assertThat(actual, is("hogehogehogehogehoge"));
+    }
+
+    @Test
+    public void repeat3() {
+        // exercise
+        String actual = Main.repeat("Test", 3);
+
+        // verify
+        assertThat(actual, is("TestTestTest"));
+    }
+
+}
